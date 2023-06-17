7f0ba07344b84e261e4424acc9d9c367979b5127
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-06-17 08:10:26 +0100

    mist and perfumes schema

--- berrysshop/schemas/index.js
+++ berrysshop/schemas/index.js
@@ -1,5 +1,7 @@
 import product from './product'
 import bodySpray from './bodySpray'
 import banner from './banner'
+import mists from './mists'
+import perfumes from './perfumes'
 
 export const schemaTypes = [product, banner, bodySpray]
