c160f0cb48267d1c74145b43fe745f72ae2ee191
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-10-27 10:51:31 +0100

    new breakpoint

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -29,14 +29,23 @@ const HeroBanner = ({ heroBanner }) => {
         </div>
       </div>
 
-      <div className="bg-red-500 flex flex-col md:flex-row justify-between items-center min-h-[500px] mt-[60px] rounded-[15px]">
-        <div>
+      <div className="bg-green-500 flex flex-col md:flex-row md:gap-[30px] md:items-center min-h-[500px] mt-[60px] rounded-[15px] leading-[0.9]">
+        <div className="pl-[15px] md:pl-[40px] pt-[90px]">
           <p className="text-[15px] md:text-[20px]">{heroBanner.smallText}</p>
-          <h3 className="text-[2rem] md:text-[4rem]">{heroBanner.midText}</h3>
-          <h1>{heroBanner.largeText1}</h1>
+          <h3 className="text-[2rem] md:text-[4rem] mt-[4px]">
+            {heroBanner.midText}
+          </h3>
+          <h1 className="text-white uppercase text-[5rem] md:text-[10rem]">
+            {heroBanner.largeText1}
+          </h1>
           <div>
             <Link href={`product/${heroBanner.product}`}>
-              <button type="button">{heroBanner.buttonText}</button>
+              <button
+                className="rounded-[15px] px-[16px] py-[10px] bg-[#f02d34] text-white mt-[40px] text-[18px] font-medium "
+                type="button"
+              >
+                {heroBanner.buttonText}
+              </button>
             </Link>
           </div>
         </div>
@@ -47,6 +56,15 @@ const HeroBanner = ({ heroBanner }) => {
             src={urlFor(heroBanner.image)}
             alt="Perfume"
           />
+        </div>
+
+        <div className="pl-[15px] py-[20px] bg-red-600 h-full">
+          <div className="flex flex-col">
+            <h6 className="mb-[12px] font-bold text-[16px] md:self-end text-[#deb887]">
+              Description
+            </h6>
+            <p className="font-thin text-[#f5f5f5]">{heroBanner.desc}</p>
+          </div>
         </div>
       </div>
     </>
